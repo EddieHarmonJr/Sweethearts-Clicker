174f792b9077d02a038d64bc64370dbc1ca87f72
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-02-06 16:21:11 -0500

    add counter function to monitor number of clicks on heart.

--- sweetheart-clicker/src/App.js
+++ sweetheart-clicker/src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      heart: 0
+    }
+  }
+
+  handleClicker = () => {
+    this.setState({
+      heart: this.state.heart + 1
+    })
+      // return (
+      //   <div>
+      //       <ul>
+      //        <li>{this.state.heart}</li>
+      //       </ul>
+      //  </div>
+      // )
+    }
+  
+
   render() {
     return (
       <div>
-        <h1>This is our Sweethearts Clicker!!!</h1>
-        <button>Sweetheart Goes Here</button>
+        <h1>❤️This is our Sweethearts Clicker!!!❤️</h1>
+        <button onClick={this.handleClicker}>Sweetheart Goes Here</button>
+        <p># OF CLICKS {this.state.heart}</p>
       </div>
     );
   }
